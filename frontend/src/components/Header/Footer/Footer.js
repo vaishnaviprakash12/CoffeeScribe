7529f7d5d10a925704cddf339064af9313f767a7
Vaishnavi Prakash <98525625+vaishnaviprakash12@users.noreply.github.com>
2024-08-04 10:43:21 +0530

    Update Footer.js

--- frontend/src/components/Header/Footer/Footer.js
+++ frontend/src/components/Header/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'; // Import your CSS file
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Copyright &copy; NoteQuill</p>
+      <p>Copyright &copy; CoffeeScribe</p>
     </div>
   );
 }
